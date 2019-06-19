30f2d6a948fe057c329c9db5fedb1eb6af1e81f3
masashi.kusunoki <fatemk73@icloud.com>
2019-06-19 04:37:56 +0000

    post

---
+++ resources/views/layouts/compzatudan.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+
+        <title>投稿完了</title>
+    </head>
+    <body>
+        <header>
+           <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                <a href="#" class="navbar-brand">AitouSearch</a>
+                
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+            </nav> 
+        </header>
+        <div class="container">
+            <h1 class="mt-4 pb-4 border-bottom">投稿完了</h1>
+            <p>ありがとうございました。投稿を受け付けました。</p>
+            <div class="text-center mb-4">
+                <a href="zatudans.index" class="btn btn-success">戻る</a>
+            </div>
+        </div>
+        <footer class="text-center pt-3 border-top">
+            &copy; 2018 SAMPLE Inc.
+        </footer>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS, then Font Awesome -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    </body>
+</html>
