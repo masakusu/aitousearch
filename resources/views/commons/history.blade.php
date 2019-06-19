39ae08fdb9392f948ea2ac4a663185cc0acc0023
masashi.kusunoki <fatemk73@icloud.com>
2019-06-19 07:57:54 +0000

    addition

---
+++ resources/views/commons/history.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>日本刀の歴史</h1>
+        </div>
+    </div>
+@endsection
